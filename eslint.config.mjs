6cd8808c2c2c0ff188eff918a80437c387465448
3x10ce <hiro.h.324@gmail.com>
2025-06-19 20:35:52 +0900

    Prettier 導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
@@ -12,4 +12,5 @@ export default defineConfig([
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  eslintConfigPrettier,
 ]);
